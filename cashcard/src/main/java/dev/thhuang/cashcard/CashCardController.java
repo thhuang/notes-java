bc3ffce487d8f8173bc9c00ad441ee4ae6deee01
TH Huang <tzhsn.huang@gmail.com>
2023-06-02 20:09:49 -0500

    update

--- cashcard/src/main/java/dev/thhuang/cashcard/CashCardController.java
+++ cashcard/src/main/java/dev/thhuang/cashcard/CashCardController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +27,10 @@ public class CashCardController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PostMapping
+    private ResponseEntity createCashCard() {
+        return null;
     }
 }
--- cashcard/src/test/java/dev/thhuang/cashcard/CashCardApplicationTests.java
+++ cashcard/src/test/java/dev/thhuang/cashcard/CashCardApplicationTests.java
@@ -1,5 +1,7 @@
 package dev.thhuang.cashcard;
 
+import java.net.URI;
+
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
@@ -41,6 +43,10 @@ class CashCardApplicationTests {
     void shouldCreateANewCashCard() {
         CashCard newCashCard = new CashCard(null, 250.00);
         ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
-        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+        URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+        ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 }
