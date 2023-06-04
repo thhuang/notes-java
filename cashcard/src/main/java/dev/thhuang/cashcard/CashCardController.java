6290cf5377550044d57d60bf1837f7765f0e471d
TH Huang <tzhsn.huang@gmail.com>
2023-06-04 15:17:27 -0500

    update

--- cashcard/src/main/java/dev/thhuang/cashcard/CashCardController.java
+++ cashcard/src/main/java/dev/thhuang/cashcard/CashCardController.java
@@ -52,6 +52,9 @@ public class CashCardController {
     @PutMapping("/{requestedId}")
     private ResponseEntity<Void> putCashCard(@PathVariable Long requestedId, @RequestBody CashCard cashCardUpdate, Principal principal) {
         CashCard cashCard = cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
+        if (cashCard == null) {
+            return ResponseEntity.notFound().build();
+        }
         CashCard updatedCashCard = new CashCard(cashCard.id(), cashCardUpdate.amount(), principal.getName());
         cashCardRepository.save(updatedCashCard);
         return ResponseEntity.noContent().build();
