e6f841af6e9c2f1775e20ae005d662998f2158e5
TH Huang <tzhsn.huang@gmail.com>
2023-06-04 15:11:30 -0500

    update

--- cashcard/src/test/java/dev/thhuang/cashcard/CashCardApplicationTests.java
+++ cashcard/src/test/java/dev/thhuang/cashcard/CashCardApplicationTests.java
@@ -170,5 +170,16 @@ class CashCardApplicationTests {
                 .withBasicAuth("sarah1", "abc123")
                 .exchange("/cashcards/99", HttpMethod.PUT, request, Void.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+
+        ResponseEntity<String> getResponse = restTemplate
+                .withBasicAuth("sarah1", "abc123")
+                .getForEntity("/cashcards/99", String.class);
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+        DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+        Number id = documentContext.read("$.id");
+        Double amount = documentContext.read("$.amount");
+        assertThat(id).isEqualTo(99);
+        assertThat(amount).isEqualTo(19.99);
     }
+
 }
