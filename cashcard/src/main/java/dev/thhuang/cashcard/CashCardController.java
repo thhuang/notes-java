3be0d05fcc27befe28431e62d653c28b20fa697c
TH Huang <tzhsn.huang@gmail.com>
2023-06-04 15:21:00 -0500

    update

--- cashcard/src/main/java/dev/thhuang/cashcard/CashCardController.java
+++ cashcard/src/main/java/dev/thhuang/cashcard/CashCardController.java
@@ -25,7 +25,7 @@ public class CashCardController {
     @GetMapping("/{requestedId}")
     public ResponseEntity<CashCard> findById(@PathVariable Long requestedId, Principal principal) {
         // Principal.name() will return the username provided from Basic Auth
-        CashCard cashCard = cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
+        CashCard cashCard = findCashCard(requestedId, principal);
         if (cashCard == null) {
             return ResponseEntity.notFound().build();
         }
@@ -54,12 +54,16 @@ public class CashCardController {
 
     @PutMapping("/{requestedId}")
     private ResponseEntity<Void> putCashCard(@PathVariable Long requestedId, @RequestBody CashCard cashCardUpdate, Principal principal) {
-        CashCard cashCard = cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
+        CashCard cashCard = findCashCard(requestedId, principal);
         if (cashCard == null) {
             return ResponseEntity.notFound().build();
         }
         CashCard updatedCashCard = new CashCard(cashCard.id(), cashCardUpdate.amount(), principal.getName());
         cashCardRepository.save(updatedCashCard);
         return ResponseEntity.noContent().build();
+    }
+
+    private CashCard findCashCard(Long requestedId, Principal principal) {
+        return cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
     }
 }
